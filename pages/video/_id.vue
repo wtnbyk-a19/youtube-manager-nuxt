cb6a12a14c46170b183074506b1d8c42d6456f83
Yuki Watanabe <>
2021-01-16 18:00:46 +0900

    Add Related Video

--- pages/video/_id.vue
+++ pages/video/_id.vue
@@ -32,6 +32,26 @@
                     <p>
                         <span>関連動画</span>
                     </p>
+                    <div v-for="relatedItem in relatedItems" :key='relatedItem.id'>
+                        <hr>
+                        <nuxt-link
+                            :to="`/video/${relatedItem.id.videoId}`"
+                        >
+                            <article class="media">
+                                <div class="media-left">
+                                    <figure>
+                                        <img :src="relatedItem.snippet.thumbnails.default.url" alt="thumbnail">
+                                    </figure>
+                                </div>
+                                <div class="media-content">
+                                    <div class="content">
+                                        <p>{{ relatedItem.snippet.title }}</p>
+                                        <small>{{ relatedItem.snippet.channelTitle}}</small>
+                                    </div>
+                                </div>
+                            </article>
+                        </nuxt-link>
+                    </div>
                 </div>
             </div>
 
@@ -47,11 +67,17 @@
             item() {
                 return this.$store.getters.getVideo
             },
+            relatedItems() {
+                return this.$store.getters.getRelatedVideos
+            },
         },
 
         async fetch({store, route}) {
             await store.dispatch('findVideo', {
                 uri: ROUTES.GET.VIDEO.replace(':id', route.params.id),
+            }),
+            await store.dispatch('fetchRelatedVideos', {
+                uri: ROUTES.GET.RELATED.replace(':id', route.params.id),
             })
         }
     }
--- store/index.js
+++ store/index.js
@@ -2,6 +2,7 @@ import {createRequestClient} from '~/store/request-client';
 
 export const state = () => ({
     items: [],
+    relatedItems: [],
     item: {},
     meta: {},
 })
@@ -21,6 +22,12 @@ export const actions = {
         }
         commit('mutateVideo', params)
     },
+
+    async fetchRelatedVideos({commit}, payload) {
+        const client = createRequestClient(this.$axios)
+        const res = await client.get(payload.uri)
+        commit('mutateRelatedVideos', res)
+    }
 }
 
 export const mutations = {
@@ -33,16 +40,26 @@ export const mutations = {
         const params = (payload.items && payload.items.length > 0) ? payload.items[0] : {}
         state.item = params
     },
+
+    mutateRelatedVideos(state, payload) {
+        state.relatedItems = payload.items || []
+    },
 }
 
 export const getters = {
     getPopularVideos(state) {
         return state.items
     },
+
     getMeta(state) {
         return state.meta
     },
+
     getVideo(state) {
         return state.item
+    },
+
+    getRelatedVideos(state) {
+        return state.relatedItems
     },
 }
