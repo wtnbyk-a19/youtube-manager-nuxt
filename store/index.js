b8c94e131cba084986b4a6482075001909e76785
Yuki Watanabe <>
2021-01-16 16:17:25 +0900

    Create Video Playing Page

--- store/index.js
+++ store/index.js
@@ -2,7 +2,8 @@ import {createRequestClient} from '~/store/request-client';
 
 export const state = () => ({
     items: [],
-        meta: {},
+    item: {},
+    meta: {},
 })
 
 export const actions = {
@@ -11,12 +12,26 @@ export const actions = {
         const res = await client.get(payload.uri, payload.params)
         commit('mutatePopularVideos', res)
     },
+
+    async findVideo({commit}, payload) {
+        const client = createRequestClient(this.$axios)
+        const res = await client.get(payload.uri)
+        const params = {
+            ...res.video_list,
+        }
+        commit('mutateVideo', params)
+    },
 }
 
 export const mutations = {
     mutatePopularVideos(state, payload) {
         state.items = payload.items ? state.items.concat(payload.items) : []
         state.meta = payload
+    },
+
+    mutateVideo(state, payload) {
+        const params = (payload.items && payload.items.length > 0) ? payload.items[0] : {}
+        state.item = params
     },
 }
 
@@ -26,5 +41,8 @@ export const getters = {
     },
     getMeta(state) {
         return state.meta
+    },
+    getVideo(state) {
+        return state.item
     },
 }
